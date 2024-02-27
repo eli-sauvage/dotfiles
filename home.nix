e07429b561d1f18e146bcd1e4a65cb618f955781
eli-sauvage <elicolh@gmail.com>
2024-02-27 16:58:39 +0100

    right version for home-manager file

--- home.nix
+++ home.nix
@@ -60,10 +60,6 @@
   #
   # or
   #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
   #  /etc/profiles/per-user/eli/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
