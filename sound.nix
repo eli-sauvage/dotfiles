cfd709bef387a63394d8160898b3928d72fd61d1
eli-sauvage <elicolh@gmail.com>
2024-02-28 11:40:38 +0100

    separate sound

---
+++ sound.nix
@@ -0,0 +1,20 @@
+{...}:{
+  # bluetooth stuff
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+ };
+}
