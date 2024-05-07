327b15caae81d46e5bbd297c11af08db7d255c34
Eli Sauvage <36312455+eli-sauvage@users.noreply.github.com>
2024-05-07 15:28:35 +0800

    Update build-iso.sh

--- .github/workflows/build-iso.sh
+++ .github/workflows/build-iso.sh
@@ -1,2 +1,4 @@
 #chown $(id -u):$(id -g) -R .
 nix build .#nixosConfigurations.live.config.system.build.isoImage --extra-experimental-features nix-command --extra-experimental-features flakes
+mkdir -p /out
+cp result/iso/*.iso /out
