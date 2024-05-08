0f28ee8af7f201d6b641f7a5e7d6bf9bb178c4ae
eli-sauvage <elicolh@gmail.com>
2024-05-08 14:54:23 +0800

    double def of xserver enable

--- machines/iso/configuration-iso.nix
+++ machines/iso/configuration-iso.nix
@@ -44,7 +44,7 @@
     packages = with pkgs; [];
     shell = pkgs.zsh;
   };
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
   #services.xserver.displayManager.sddm.enable = true;
   #services.xserver.desktopManager.plasma5.enable = true;
   #
