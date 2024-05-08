eb3983c6c66eb31a0aaf40083a4ba3dd350716f1
eli-sauvage <elicolh@gmail.com>
2024-05-08 23:42:03 +0800

    typo in cp iso

--- .github/workflows/build-iso.sh
+++ .github/workflows/build-iso.sh
@@ -2,4 +2,4 @@
 #nix build .#nixosConfigurations.live.config.system.build.isoImage --extra-experimental-features nix-command --extra-experimental-features flakes
 nix build .#iso --extra-experimental-features nix-command --extra-experimental-features flakes
 mkdir -p /out
-cp -r result/*.iso /out
+cp result/iso/*.iso /out
