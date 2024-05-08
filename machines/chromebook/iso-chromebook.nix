d2ccec7779287bc322975a8b2a694a2f361b8824
eli-sauvage <elicolh@gmail.com>
2024-05-08 22:46:30 +0800

    channel ok?

--- machines/chromebook/iso-chromebook.nix
+++ machines/chromebook/iso-chromebook.nix
@@ -9,7 +9,7 @@
 {
   imports = [
     ./hardware-config-iso.nix
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
   isoImage.compressImage = true;
 
