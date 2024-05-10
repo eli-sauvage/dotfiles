07bec5caf17e5f263cdada7c898d0721aee3153a
Eli Sauvage <36312455+eli-sauvage@users.noreply.github.com>
2024-05-10 20:38:16 +0000

    rm comment

--- machines/iso/iso-configuration.nix
+++ machines/iso/iso-configuration.nix
@@ -55,11 +55,6 @@
     packages = with pkgs; [];
     shell = pkgs.zsh;
   };
-  #services.xserver.enable = true;
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
-  #
-  #services.xserver.displayManager.defaultSession = "plasmawayland";
   services.xserver = {
     enable = true;
     desktopManager = {
