07a66ddb2fbbb2c371fd9aaff134f39fdd8cb1f4
eli-sauvage <elicolh@gmail.com>
2024-10-03 11:37:54 +0200

    video driver stuff

--- machines/dell-g3/graphics.nix
+++ machines/dell-g3/graphics.nix
@@ -1,12 +1,17 @@
-{config, lib, pkgs, ...}:{
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   # hardware.graphics.enable = true;
-  # services.xserver.videoDrivers = ["intel"];
+  # services.xserver.videoDrivers = ["intel" "nvidia"];
   # hardware.nvidia = {
   #   modesetting.enable = true;
   #   powerManagement.enable = false;
   #   powerManagement.finegrained = false;
   #   open = false;
   #   nvidiaSettings = true;
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #   # package = config.boot.kernelPackages.nvidiaPackages.stable;
   # };
 }
