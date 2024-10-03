f666a94c39c63b0778ccf2a048de4ecd6220a294
eli-sauvage <elicolh@gmail.com>
2024-10-03 11:36:22 +0200

    tab = 4 spaces

--- home-manager/neovim/lua/options.lua
+++ home-manager/neovim/lua/options.lua
@@ -13,9 +13,9 @@ vim.opt.autoread = true
 
 --expand tab to 4 spaces
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 --don't load netrw
 vim.g.loaded_netrw = 1
