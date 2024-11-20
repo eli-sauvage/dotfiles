94562ec167b10e5f56806ad6b62557e81eedae8f
eli-sauvage <elicolh@gmail.com>
2024-11-20 10:11:45 +0100

    retry nvidia settings

--- machines/dell-g3/graphics.nix
+++ machines/dell-g3/graphics.nix
@@ -4,14 +4,18 @@
   pkgs,
   ...
 }: {
-  # hardware.graphics.enable = true;
-  # services.xserver.videoDrivers = ["intel" "nvidia"];
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   powerManagement.enable = false;
-  #   powerManagement.finegrained = false;
-  #   open = false;
-  #   nvidiaSettings = true;
-  #   # package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+  # temporay for for bottles error: Gdk-Message: 11:58:31.996: Error 71 (Protocol error) dispatching to Wayland display.
+  # follow https://github.com/NixOS/nixpkgs/issues/353990
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["intel" "nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
